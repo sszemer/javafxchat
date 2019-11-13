06127be125fd2e268b047e7e548dc83c6ac2aead
sszemer <sszemer@gmail.com>
2019-11-13 18:03:40 +0100

    prawie gotowe

--- src/main/java/FXMLController.java
+++ src/main/java/FXMLController.java
@@ -4,6 +4,12 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Arrays;
+
 
 public class FXMLController {
 
@@ -18,14 +24,36 @@ public class FXMLController {
 
     @FXML
     private Button send;
+    private InetAddress address;
+    private DatagramSocket datagramSocket;
 
     public void addLine(String text){
-        this.text.appendText(text);
+        this.text.setText(this.text.getText().concat(text) + "\n");
     }
+
+    //zrobic liste userow
+
     @FXML
-    public void onBtClick(javafx.event.ActionEvent actionEvent) {
+    public void onBtClick(javafx.event.ActionEvent actionEvent) throws IOException {
         String text = line.getText();
         line.setText("");
         this.text.appendText(text+"\n");
+
+        byte[] buf = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 6666);
+        byte[] name = Arrays.copyOf("Sebastian".getBytes(),30);
+        byte[] data = Arrays.copyOf(this.text.getText().getBytes(),1024-30);
+        System.arraycopy(name,0,buf,0,30);
+        System.arraycopy(data,0,buf,31,993);
+        packet.setData(buf);
+        datagramSocket.send(packet);
+    }
+
+    public void setAddress(InetAddress address) {
+        this.address=address;
+    }
+
+    public void setSocket(DatagramSocket datagramSocket) {
+        this.datagramSocket=datagramSocket;
     }
 }
